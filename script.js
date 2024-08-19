5fc33515d6399ee537bdab755c6340ab3225648f
Mark Lester Raguindin <104201271+Mlester20@users.noreply.github.com>
2024-08-19 22:47:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    let currentValue = document.getElementById('display').value;
+    document.getElementById('display').value = currentValue.slice(0, -1);
+}
+
+function calculate() {
+    document.getElementById('display').value = 'Pak u bro';
+}
